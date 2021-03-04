d2885d76080dbbd10c5b5660705933d29a71ee2b
Joaquim Verdasca <joaquimnrv@gmail.com>
2021-03-04 22:20:55 +0000

    Added voice

--- src/commands/Catchphrase.command.ts
+++ src/commands/Catchphrase.command.ts
@@ -1,8 +1,25 @@
 import { Command } from "./Command";
+import _ from "underscore";
 
 export class CatchphraseCommand extends Command {
+    private PHRASES = [
+        "./assets/voice/catchphrase.mp3",
+        "./assets/voice/catchphraseportugues.mp3"
+    ];
+
     public async execute(): Promise<void> {
         await this.message.channel.send("You're the best!");
+
+        if (this.message.member?.voice.channel) {
+            const voiceConnection = await this.message.member.voice.channel.join();
+            const dispatcher = voiceConnection.play(_.sample(this.PHRASES)!, {
+                volume: 1
+            });
+
+            dispatcher.on("finish", () => {
+                voiceConnection.disconnect();
+            });
+        }
     }
 
     public canExecute(): boolean {
